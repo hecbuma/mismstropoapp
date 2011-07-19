3a13492304008f52272904b4a11625a07decd492
Hector Bustillos <hector.bustillos@crowdint.com>
2011-07-19 00:48:55 -0500

    fix typo

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,24 +1,20 @@
 class MessagesController < ApplicationController
-end
-
-
   # Process an SMS message request from Tropo
   def sms
     #values = params[:session][:parameters]
-    request_token = 06a748a14827484b94cf6a54ae796f5dcca4ca9ee1c87810470d6efaba46681a1303749943b18bb4b55faf7c
-    request_user = 534
+    request_token = '06a748a14827484b94cf6a54ae796f5dcca4ca9ee1c87810470d6efaba46681a1303749943b18bb4b55faf7c'
+    request_user = '534'
     request_ticket = Time.now.to_i
 
     # Generate Tropo response 
     t = Tropo::Generator.new
     t.call(:to => '+523121194800', :network => "SMS")
-    t.say(:value => "it's aliveee!")
+    t.say(:value => "its aliveee!")
     tropo_message = t.response
 
     respond_to do |format|
       format.html  { redirect_to root_path }
       format.json  { render :json => tropo_message }
     end
-  end
-
+  end  
 end
