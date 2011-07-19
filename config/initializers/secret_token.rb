58893bd3f3f3146ff8fae5fc821752387db09602
Hector Bustillos <hector.bustillos@crowdint.com>
2011-07-19 00:40:01 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tropo::Application.config.secret_token = 'b687d9b5dccf9874425a8f2515d4289007e5fd2caeab2f6de6480d0a463aedec313f39deb8311430c01470ac572167748f438d2cff9eb6955695f6eb481df05e'
